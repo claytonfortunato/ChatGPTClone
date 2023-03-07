7d41f86169cc34487a6364b15c848fb1cefd714b
claytonfortunato <claytonreis10@gmail.com>
2023-03-06 23:07:11 -0300

    feat: busca pela api

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import * as C from "./styles";
 
 const App = () => {
   const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState("");
+
   const configuration = new Configuration({
     apiKey: import.meta.env.REACT_APP_KEY,
   });
@@ -14,19 +16,25 @@ const App = () => {
 
   const generateImage = async () => {
     const res = await openai.createImage({
-      prompt: "Say this is a test",
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
 
-    console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);
   };
 
   return (
     <C.Container>
       <C.Header>Gerar uma imagem usando a API Open AI</C.Header>
-      <input type="text" />
+      <input
+        placeholder="Pesquisar imagem"
+        type="text"
+        onChange={(e) => setPrompt(e.target.value)}
+      />
       <C.Button onClick={generateImage}>Gerar uma Imagem</C.Button>
+
+      {result.length > 0 ? <img src={result} alt="result" /> : <></>}
     </C.Container>
   );
 };
